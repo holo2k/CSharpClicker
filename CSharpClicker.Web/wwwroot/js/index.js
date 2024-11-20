f7d7703aa511d28c66bcf61cfd439ec6ebb65221
holo2k <nneketaa@yandex.ru>
2024-11-20 19:36:02 +0500

    orc image add

---
+++ CSharpClicker.Web/wwwroot/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var clickItem = document.getElementById("clickitem");
+
+    clickitem.onclick = click();
+})
+
+function click() {
+    console.log("asdasd")
+}
